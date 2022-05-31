1f3b7b667e7431dbad1d2b703ca7ebbac1a0a6a7
Sajjad Sanikhani <sssanikhani@gmail.com>
2022-05-31 18:24:16 +0430

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import minio
 m = minio.Minio(
     "192.168.1.121:9000",
     access_key="sssanikhani",
-    secret_key="Sajjad326495S",
+    secret_key="sssanikhani",
     secure=False
 )
 obj = m.fget_object("profileimages", "1.jpg", "/home/sss/sss/1.jpg")
